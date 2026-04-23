f405b61f128e5651adf07666234cbc83b86c4b3c
chandro4D <chandroshakhor7@gmail.com>
2026-04-23 23:42:45 +0600

    Update user dashboard

--- frontend/src/Dashboard/DashboardRoute/Dashboard.jsx
+++ frontend/src/Dashboard/DashboardRoute/Dashboard.jsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import Admin from '../AdminDashboard/AdminHome';
-import User from '../UserDashboard/User';
-import Seller from '../SellerDashboard/Seller';
-import Cart from '../../pages/Cart/Cart';
-import { NavLink, Outlet } from 'react-router-dom';
-import { FaBook, FaCartPlus, FaEnvelope, FaHome, FaList, FaUsers, FaUser } from "react-icons/fa";
+import React from "react";
+import Admin from "../AdminDashboard/AdminHome";
+import User from "../UserDashboard/User";
+import Seller from "../SellerDashboard/Seller";
+import Cart from "../../pages/Cart/Cart";
+import { NavLink, Outlet } from "react-router-dom";
+import {
+  FaBook,
+  FaCartPlus,
+  FaEnvelope,
+  FaHome,
+  FaList,
+  FaUsers,
+  FaBox,
+  FaHeart,
+  FaWallet,
+  FaUser,
+  FaMapMarkerAlt,
+  FaHeadset
+} from "react-icons/fa";
 import { MdOutlineManageAccounts, MdRestaurantMenu } from "react-icons/md";
 
 function Dashboard() {
-    const userString = localStorage.getItem("user");
-    const user = userString ? JSON.parse(userString) : null;
+  const userString = localStorage.getItem("user");
+  const user = userString ? JSON.parse(userString) : null;
 
-    const role = user?.role;
+  const role = user?.role;
 
-    return (
-        <div className="flex  ml-0">
-            <div className="w-[280px] min-h-screen bg-gradient-to-r from-indigo-500  to-pink-500">
-                <ul className="menu pl-8  text-white">
-                    {/* {user?.PhotoURL ? (
+  return (
+    <div className="flex  ml-0">
+      <div className="w-[280px] min-h-screen bg-gradient-to-r from-indigo-500  to-pink-500">
+        <ul className="menu pl-8  text-white">
+          {/* {user?.PhotoURL ? (
                         <img
                             className="mr-2 border-2 object-cover border-white ml-16 mb-2 w-[80px] h-[80px] rounded-full"
                             src={user.PhotoURL}
@@ -26,93 +39,104 @@ function Dashboard() {
                     ) : (
                         <FaUser className="mr-2 ml-16 mb-2 w-20 h-20 text-white" />
                     )} */}
-                    <div className="pt-8">
-                        <img className="mr-2 border-4 object-cover border-indigo-400 ml-16 mb-2 w-[80px] h-[80px] rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3A9J0P677Sulx-vF3NY5-T3zdAz_Z5sZZCwhdfhSW8CY3aR8Ng2-TpfU&s" alt="" />
-                        <h1 className="text-xl pl-9 mb-3 font-bold">Smart Agro Hub</h1>
-                    </div>
-                    {role === "Admin" && (
-                        <>
-                            <li>
-                                <NavLink to="/dashboard/adminHome">
-                                    <FaHome /> Dashboard
-                                </NavLink>
-                            </li>
+          <div className="pt-8">
+            <img
+              className="mr-2 border-4 object-cover border-indigo-400 ml-16 mb-2 w-[80px] h-[80px] rounded-full"
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3A9J0P677Sulx-vF3NY5-T3zdAz_Z5sZZCwhdfhSW8CY3aR8Ng2-TpfU&s"
+              alt=""
+            />
+            <h1 className="text-xl pl-9 mb-3 font-bold">Smart Agro Hub</h1>
+          </div>
+          {role === "Admin" && (
+            <>
+              <li>
+                <NavLink to="/dashboard/adminHome">
+                  <FaHome /> Dashboard
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/adminAnalytics">
-                                    <FaList /> Analytics
-                                </NavLink>
-                            </li>
+              <li>
+                <NavLink to="/dashboard/adminAnalytics">
+                  <FaList /> Analytics
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/manageUsers">
-                                    <FaUsers /> Manage Users
-                                </NavLink>
-                            </li>
+              <li>
+                <NavLink to="/dashboard/manageUsers">
+                  <FaUsers /> Manage Users
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/manageProducts">
-                                    <FaCartPlus /> Manage Products
-                                </NavLink>
-                            </li>
+              <li>
+                <NavLink to="/dashboard/manageProducts">
+                  <FaCartPlus /> Manage Products
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/manageCategory">
-                                    <MdOutlineManageAccounts /> Manage Categories
-                                </NavLink>
-                            </li>
+              <li>
+                <NavLink to="/dashboard/manageCategory">
+                  <MdOutlineManageAccounts /> Manage Categories
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/orders">
-                                    <FaList /> Orders & Payments
-                                </NavLink>
-                            </li>
+              <li>
+                <NavLink to="/dashboard/orders">
+                  <FaList /> Orders & Payments
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/adminSalesReport">
-                                    <FaBook /> Sales Report
-                                </NavLink>
-                            </li>
+              <li>
+                <NavLink to="/dashboard/adminSalesReport">
+                  <FaBook /> Sales Report
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/adminManageBanner">
-                                    <FaBook /> Content Management
-                                </NavLink>
-                            </li>
+              <li>
+                <NavLink to="/dashboard/adminManageBanner">
+                  <FaBook /> Content Management
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/notifications">
-                                    <FaEnvelope /> Notifications
-                                </NavLink>
-                            </li>
+              <li>
+                <NavLink to="/dashboard/notifications">
+                  <FaEnvelope /> Notifications
+                </NavLink>
+              </li>
 
-                            <li>
-                                <NavLink to="/dashboard/settings">
-                                    <MdOutlineManageAccounts /> Settings
-                                </NavLink>
-                            </li>
-                        </>
-                    )}
+              <li>
+                <NavLink to="/dashboard/settings">
+                  <MdOutlineManageAccounts /> Settings
+                </NavLink>
+              </li>
+            </>
+          )}
 
-                    {role === "Seller" && (
+          {role === "Seller" && (
+            <>
+              <li>
+                <NavLink to="/seller">
+                  <FaHome></FaHome> Seller Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/manageMedicine">
+                  <FaCartPlus></FaCartPlus> Manage Medicine
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/sellerPaymentHistory">
+                  <FaCartPlus></FaCartPlus> Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/sellerAd">
+                  <FaCartPlus></FaCartPlus> Advertisement
+                </NavLink>
+              </li>
+            </>
+          )}
 
-                        <>
-                            <li>
-                                <NavLink to="/seller"><FaHome></FaHome> Seller Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/dashboard/manageMedicine'><FaCartPlus></FaCartPlus> Manage Medicine</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/dashboard/sellerPaymentHistory'><FaCartPlus></FaCartPlus> Payment History</NavLink>
-                            </li>
-                            <li>
-                                <NavLink to='/dashboard/sellerAd'><FaCartPlus></FaCartPlus> Advertisement</NavLink>
-                            </li>
-                        </>
-                    )}
-
-                    {role === "user" && (
+          {/* {role === "user" && (
                         <>
                             <li>
                                 <NavLink to="/dashboard/user"><FaHome></FaHome> User Home</NavLink>
@@ -125,36 +149,81 @@ function Dashboard() {
                             </li>
                         </>
 
-                    )}
+                    )} */}
 
-                    {!role && (
-                        <h2 className='text-2xl text-gray-600'>
-                            Please login
-                        </h2>
-                    )}
+          {role === "user" && (
+            <>
+              <li>
+                <NavLink to="/dashboard/user">
+                  <FaHome /> Dashboard
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/orders">
+                  <FaBox /> My Orders
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/cart">
+                  <FaCartPlus /> My Cart
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/wishlist">
+                  <FaHeart /> Wishlist
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/paymentHistory">
+                  <FaWallet /> Payment History
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/profile">
+                  <FaUser /> My Profile
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/address">
+                  <FaMapMarkerAlt /> Address Book
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/dashboard/support">
+                  <FaHeadset /> Support
+                </NavLink>
+              </li>
+            </>
+          )}
 
+          {!role && <h2 className="text-2xl text-gray-600">Please login</h2>}
 
-
-
-                    {/* -----------------------------sherd item------------------------------------------------------------ */}
-                    <div className="divider divider-neutral"></div>
-                    <li>
-                        <NavLink to="/"><FaHome></FaHome>  Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/shop"><MdRestaurantMenu />Shop</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact"><FaEnvelope />Contact</NavLink>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <Outlet></Outlet>
-            </div>
-
-        </div>
-    );
+          {/* -----------------------------sherd item------------------------------------------------------------ */}
+          <div className="divider divider-neutral"></div>
+          <li>
+            <NavLink to="/">
+              <FaHome></FaHome> Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/shop">
+              <MdRestaurantMenu />
+              Shop
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">
+              <FaEnvelope />
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <Outlet></Outlet>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
